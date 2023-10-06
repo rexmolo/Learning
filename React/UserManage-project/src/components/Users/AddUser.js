445f6a2a4df8e8c2f6fb58bc3df6a5627de3db24
rexmolo <jackie.hui.ma@gmail.com>
2023-10-06 09:42:53 +0200

    fragment

--- React/UserManage-project/src/components/Users/AddUser.js
+++ React/UserManage-project/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import {useState} from "react";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
 
@@ -48,7 +49,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && (<ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>)}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -59,7 +60,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
 
     );
 }
